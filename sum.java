9023caeb69d003f0d377d4fc6bbfaf7d1acab6e5
vanarpcoding <129204429+vanarpcoding@users.noreply.github.com>
2023-03-28 21:41:04 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum
+{
+public static void main()
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the two numbers a and b ");
+a = sc.nextInt();
+b = sc.nextInt();
+int sum = a+b;
+System.out.println("The sum =  " + sum);
+} //closing main
+}//closing class
